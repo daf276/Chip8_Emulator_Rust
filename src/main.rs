7a0ad5fca9e23d92bddf743f0992e739b9d2f20b
Attila <daf276@gmx.de>
2020-01-24 00:54:04 +0100

    SDL windows is created, nothing drawn yet

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 extern crate clap;
 extern crate rand;
+extern crate sdl2;
 
 use clap::{App, Arg};
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
 use std::fs::File;
 use std::io::prelude::*;
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+const SCREEN_WIDTH: u32 = 64;
+const SCREEN_HEIGHT: u32 = 32;
 
 struct Opcode {
     bytes: u16,
@@ -298,10 +307,8 @@ fn main() {
         Some(f) => {
             println!("The file passed is: {}", f);
 
-            let mut file = File::open(f);
-
-            match file {
-                Ok(x) => init(x),
+            match File::open(f) {
+                Ok(file) => emulate(load_program(file)),
                 Err(_) => println!("File doesnt exist"),
             }
         }
@@ -309,13 +316,53 @@ fn main() {
     }
 }
 
-fn init(mut f: File) {
+fn emulate(mut chip: Chip8) {
+    let sdl_context = sdl2::init().expect("Can't get SDLContext");
+    let video_subsystem = sdl_context
+        .video()
+        .expect("Can't initialize video subsystem");
+    let window = video_subsystem
+        .window("Chip-8", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .position_centered()
+        .build()
+        .expect("Can't create window");
+    let mut canvas = window.into_canvas().build().expect("Can't create canvas");
+    let mut event_pump = sdl_context.event_pump().expect("Can't get event pump");
+
+    let mut quit = false;
+
+    while !&quit {
+        let before_cycle = Instant::now();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    quit = true;
+                }
+                _ => {}
+            }
+        }
+
+        chip.emulate_cycle();
+
+        let time_to_wait = (1000000000u128 / 60u128) - before_cycle.elapsed().as_nanos(); //60Fps
+        sleep(Duration::new(0, time_to_wait as u32));
+    }
+}
+
+fn load_program(mut file: File) -> Chip8 {
     let mut chip8 = Chip8::new();
 
     let mut buffer = Vec::new();
-    f.read_to_end(&mut buffer).unwrap();
+    file.read_to_end(&mut buffer).unwrap();
 
-    chip8.load_into_memory(buffer)
+    chip8.load_into_memory(buffer);
+
+    return chip8;
 }
 
 #[cfg(test)]
