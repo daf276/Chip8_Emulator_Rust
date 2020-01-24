91a4f05832ff66a3ee0c324705970f05739a9c21
Attila <daf276@gmx.de>
2020-01-24 14:40:47 +0100

    Gfx from emulator can now be displayed

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,22 @@ extern crate sdl2;
 use clap::{App, Arg};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::pixels::PixelFormatEnum;
 use std::fs::File;
 use std::io::prelude::*;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-const SCREEN_WIDTH: u32 = 64;
-const SCREEN_HEIGHT: u32 = 32;
+const SCREEN_WIDTH: usize = 64;
+const SCREEN_HEIGHT: usize = 32;
+const PITCH: usize = SCREEN_WIDTH * 4;
+const SCALE: u32 = 16;
 
 struct Opcode {
     bytes: u16,
 }
 
 impl Opcode {
-    pub fn get(&self) -> u16 {
-        self.bytes
-    }
-
     pub fn set(&mut self, upper: u8, lower: u8) {
         self.bytes = (upper as u16) << 8 | lower as u16;
     }
@@ -31,7 +30,7 @@ impl Opcode {
     }
 
     pub fn get_3bytes(&self) -> u16 {
-        self.bytes & 0x0FFF
+        (self.bytes & 0x0FFF)
     }
 
     pub fn get_2bytes(&self) -> u8 {
@@ -155,7 +154,7 @@ impl Chip8 {
             0xA => self.ldi(nnn),
             0xB => self.jump(nnn + self.v[0] as u16),
             0xC => self.ld(x, rand::random::<u8>() & nn),
-            0xD => {} //TODO
+            0xD => {} //self.display_sprite(x as u8, y as u8, n as u8),
             0xE => {} //TODO
             0xF => self.opcodef(x, y, n),
             _ => {}
@@ -469,11 +468,24 @@ fn emulate(mut chip: Chip8) {
         .video()
         .expect("Can't initialize video subsystem");
     let window = video_subsystem
-        .window("Chip-8", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .window(
+            "Chip-8",
+            SCALE * SCREEN_WIDTH as u32,
+            SCALE * SCREEN_HEIGHT as u32,
+        )
         .position_centered()
         .build()
         .expect("Can't create window");
     let mut canvas = window.into_canvas().build().expect("Can't create canvas");
+    canvas.set_scale(SCALE as f32, SCALE as f32).unwrap();
+    let texture_creator = canvas.texture_creator();
+    let mut texture = texture_creator
+        .create_texture_static(
+            PixelFormatEnum::ABGR8888,
+            SCREEN_WIDTH as u32,
+            SCREEN_HEIGHT as u32,
+        )
+        .expect("Can't create texture");
     let mut event_pump = sdl_context.event_pump().expect("Can't get event pump");
 
     let mut quit = false;
@@ -496,8 +508,14 @@ fn emulate(mut chip: Chip8) {
 
         chip.emulate_cycle();
 
-        let time_to_wait =
-            (1000000000u128 / 60u128).saturating_sub(before_cycle.elapsed().as_nanos()); //60Fps
+        let pixel_data = update_gfx(&chip.gfx);
+        texture.update(None, &pixel_data[..], PITCH).unwrap();
+
+        canvas.clear();
+        canvas.copy(&texture, None, None).unwrap();
+        canvas.present();
+
+        let time_to_wait = 16666666u128.saturating_sub(before_cycle.elapsed().as_nanos()); //60Fps
         sleep(Duration::new(0, time_to_wait as u32));
     }
 }
@@ -511,6 +529,26 @@ fn load_program(mut file: File) -> Chip8 {
     chip8.load_into_memory(buffer);
 
     return chip8;
+}
+
+fn update_gfx(emulator_gfx: &Vec<Vec<bool>>) -> Vec<u8> {
+    let mut gfx = vec![0; SCREEN_HEIGHT * SCREEN_WIDTH];
+
+    for i in 0..SCREEN_HEIGHT {
+        for j in 0..SCREEN_WIDTH {
+            let pixel = emulator_gfx[i][j] as u32 * 0xFFFFFFFF;
+            gfx[i * SCREEN_WIDTH + j] = pixel;
+        }
+    }
+
+    return split_gfx_into_color_components(gfx);
+}
+
+fn split_gfx_into_color_components(gfx: Vec<u32>) -> Vec<u8> {
+    unsafe {
+        let (_, ret, _) = gfx.align_to::<u8>();
+        return ret.to_vec();
+    }
 }
 
 #[cfg(test)]
