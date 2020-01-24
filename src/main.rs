da158532583a8264e72fdff9b65a5d298b505c1d
Attila <daf276@gmx.de>
2020-01-24 01:08:19 +0100

    Hexdigits now get loaded into memory at startup

--- src/main.rs
+++ src/main.rs
@@ -78,6 +78,7 @@ impl Programcounter {
 }
 
 struct Chip8 {
+    gfx: Vec<Vec<u8>>,
     memory: Vec<u8>,
     v: Vec<u8>,
     stack: Vec<u16>,
@@ -94,9 +95,10 @@ struct Chip8 {
 
 impl Chip8 {
     pub fn new() -> Chip8 {
-        let memory: Vec<u8> = vec![0; 4096]; //4096 bits of memory
-        let v: Vec<u8> = vec![0; 16]; //CPU registers named V0 to VE, last register is the carry flag
-        let stack: Vec<u16> = vec![0; 16]; //16 Stacklevels
+        let gfx = vec![vec![0; 64]; 32];
+        let mut memory = vec![0; 4096]; //4096 bits of memory
+        let v = vec![0; 16]; //CPU registers named V0 to VE, last register is the carry flag
+        let stack = vec![0; 16]; //16 Stacklevels
 
         let opcode = Opcode { bytes: 0 };
         let sp = 0;
@@ -107,7 +109,10 @@ impl Chip8 {
         let delay_timer = 0;
         let sound_timer = 0;
 
+        memory = Chip8::load_hex_digits(memory);
+
         Chip8 {
+            gfx,
             memory,
             v,
             stack,
@@ -287,6 +292,107 @@ impl Chip8 {
             _ => {}
         }
     }
+
+    fn load_hex_digits(mut memory: Vec<u8>) -> Vec<u8> {
+        //Zero
+        memory[0] = 0xF0;
+        memory[1] = 0x90;
+        memory[2] = 0x90;
+        memory[3] = 0x90;
+        memory[4] = 0xF0;
+        //One
+        memory[5] = 0x20;
+        memory[6] = 0x60;
+        memory[7] = 0x20;
+        memory[8] = 0x20;
+        memory[9] = 0x70;
+        //Two
+        memory[10] = 0xF0;
+        memory[11] = 0x10;
+        memory[12] = 0xF0;
+        memory[13] = 0x80;
+        memory[14] = 0xF0;
+        //Three
+        memory[15] = 0xF0;
+        memory[16] = 0x10;
+        memory[17] = 0xF0;
+        memory[18] = 0x10;
+        memory[19] = 0xF0;
+        //Four
+        memory[20] = 0x90;
+        memory[21] = 0x90;
+        memory[22] = 0xF0;
+        memory[23] = 0x10;
+        memory[24] = 0x10;
+        //Five
+        memory[25] = 0xF0;
+        memory[26] = 0x80;
+        memory[27] = 0xF0;
+        memory[28] = 0x10;
+        memory[29] = 0xF0;
+        //Six
+        memory[30] = 0xF0;
+        memory[31] = 0x80;
+        memory[32] = 0xF0;
+        memory[33] = 0x90;
+        memory[34] = 0xF0;
+        //Seven
+        memory[35] = 0xF0;
+        memory[36] = 0x10;
+        memory[37] = 0x20;
+        memory[38] = 0x40;
+        memory[39] = 0x40;
+        //Eight
+        memory[40] = 0xF0;
+        memory[41] = 0x90;
+        memory[42] = 0xF0;
+        memory[43] = 0x90;
+        memory[44] = 0xF0;
+        //Nine
+        memory[45] = 0xF0;
+        memory[46] = 0x90;
+        memory[47] = 0xF0;
+        memory[48] = 0x10;
+        memory[49] = 0xF0;
+        //A
+        memory[50] = 0xF0;
+        memory[51] = 0x90;
+        memory[52] = 0xF0;
+        memory[53] = 0x90;
+        memory[54] = 0x90;
+        //B
+        memory[55] = 0xE0;
+        memory[56] = 0x90;
+        memory[57] = 0xE0;
+        memory[58] = 0x90;
+        memory[59] = 0xE0;
+        //C
+        memory[60] = 0xF0;
+        memory[61] = 0x80;
+        memory[62] = 0x80;
+        memory[63] = 0x80;
+        memory[64] = 0xF0;
+        //D
+        memory[65] = 0xE0;
+        memory[66] = 0x90;
+        memory[67] = 0x90;
+        memory[68] = 0x90;
+        memory[69] = 0xE0;
+        //E
+        memory[70] = 0xF0;
+        memory[71] = 0x80;
+        memory[72] = 0xF0;
+        memory[73] = 0x80;
+        memory[74] = 0xF0;
+        //F
+        memory[75] = 0xF0;
+        memory[76] = 0x80;
+        memory[77] = 0xF0;
+        memory[78] = 0x80;
+        memory[79] = 0x80;
+
+        return memory;
+    }
 }
 
 fn main() {
@@ -349,7 +455,8 @@ fn emulate(mut chip: Chip8) {
 
         chip.emulate_cycle();
 
-        let time_to_wait = (1000000000u128 / 60u128) - before_cycle.elapsed().as_nanos(); //60Fps
+        let time_to_wait =
+            (1000000000u128 / 60u128).saturating_sub(before_cycle.elapsed().as_nanos()); //60Fps
         sleep(Duration::new(0, time_to_wait as u32));
     }
 }
